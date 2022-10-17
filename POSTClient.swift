b68e18d84ac69fd88c2e5fe2a33276922c710e70
Daniel <daniel.prolite@gmail.com>
2022-10-17 17:58:11 -0500

    Change minimal version

---
+++ POSTClient.swift
@@ -0,0 +1,15 @@
+//
+//  POSTClient.swift
+//  POST
+//
+//  Created by Daniel Vázquez on 17/10/22.
+//
+
+import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+
+public actor POSTClient {
+    
+}
